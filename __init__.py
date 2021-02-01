37f21ebcd8ffe22baab4bbd35c9dc38499a6b7a7
crarojasca <cristian.crojas@mercadolibre.com.co>
2021-02-01 00:26:01 -0500

    Login and User Model

---
+++ __init__.py
@@ -0,0 +1,36 @@
+from flask_login import LoginManager
+from flask import Flask, Blueprint, render_template
+
+from .models import db
+from .config import DevelopmentConfig, ProductionConfig
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models.user import User
+
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+    return app
--- config.py
+++ config.py
@@ -2,11 +2,11 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////db/local.db'
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
     SECRET_KEY = 'secret-key-goes-here'
 
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
-    ECRET_KEY = 'secret-key-goes-here'
+    SECRET_KEY = 'secret-key-goes-here'
